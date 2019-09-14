a26b1a903398611deaf74620a0804bdae9654789
zzZaida <17638121602@163.com>
2019-09-14 20:57:45 +0800

    修改订单的状态

--- meiduo_mall/apps/meiduo_admin/views/order.py
+++ meiduo_mall/apps/meiduo_admin/views/order.py
@@ -1,3 +1,4 @@
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
@@ -17,3 +18,40 @@ class OrderModelViewSet(ModelViewSet):
     def destroy(self, request, *args, **kwargs):
 
         return Response({'msg': '妖怪,吃俺老孙一棒,敢删除我的数据!'})
+
+    @action(methods=['PUT'],detail=True)
+    def status(self,request,pk):
+        # 1.查询订单
+        try:
+            order=OrderInfo.objects.get(order_id=pk)
+        except OrderInfo.DoesNotExist:
+            from rest_framework import status
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        # order=self.get_object()
+        # 2.修改订单状态
+        order.status=request.data.get('status')
+        order.save()
+        #3.返回相应
+        return Response({
+            'order_id':pk,
+            'status':order.status
+        })
+
+
+    """
+    GET
+    {
+      "order_id": "20190909155657000000003",
+      "create_time": "2019-09-09T15:56:57.524510+08:00",
+      "update_time": "2019-09-09T15:57:02.595491+08:00",
+      "total_count": 1,
+      "total_amount": "11.00",
+      "freight": "10.00",
+      "pay_method": 2,
+      "status": 1,
+      "user": 3,
+      "address": 4,
+      "goods":[{},{},{},{}]
+    }
+    """
--- meiduo_mall/static/js/user_center_site.js
+++ meiduo_mall/static/js/user_center_site.js
@@ -37,7 +37,7 @@ var vm = new Vue({
         // 将用户地址列表绑定到变量, addresses 是django模板传给vue的json字符串
         this.addresses = JSON.parse(JSON.stringify(addresses));
         // 默认地址id
-        this.default_address_id = default_address_id;
+        // this.default_address_id = default_address_id;
     },
     watch: {
         // 监听到省份id变化
@@ -92,8 +92,7 @@ var vm = new Vue({
             })
                 .then(response => {
                     if (response.data.code == '0') {
-
-                        this.provinces = response.data.provience_list;
+                        this.provinces = response.data.province_list;
                     } else {
                         console.log(response.data);
                         this.provinces = [];
@@ -130,7 +129,6 @@ var vm = new Vue({
             if (this.form_address.tel) {
                 var re = /^(0[0-9]{2,3}-)?([2-9][0-9]{6,7})+(-[0-9]{1,4})?$/;
                 if (re.test(this.form_address.tel)) {
-                    this.error_tel = false;
                     this.error_tel = false;
                 } else {
                     this.error_tel = true;
